4f9c321f6bb4daa29c700608ce7c6bd0607f511e
longoprince <longolingprince@gmail.com>
2025-06-21 02:53:09 +0100

    statlesswidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,20 @@ void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends statelessWidget{
+  const ({Key? Key}) : super(Key: Key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("immo projet")
+          ),
+          body: Center(
+            child: Text("Trouver votre prochaine chez soi"), 
+            ) //centrer texte 
+        ), //Scaffold
+      ); //MaterielApp
+  }
+}
