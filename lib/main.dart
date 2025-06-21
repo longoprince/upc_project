231b9676c3f3ad11627bf08e47ae3fe159241f20
Longo <longolongprince@gmail.com>
2025-06-21 11:47:59 +0200

    texte et appbar doré

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,53 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
-class MyApp extends statelessWidget{
-  const ({Key? Key}) : super(Key: Key);
-  
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key); // "key" doit être en minuscule ici
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("immo projet")
+          title: const Text("Immo Projet"),
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+            color: Color(0xFFFFD700),
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
           ),
-          body: Center(
-            child: Text("Trouver votre prochaine chez soi"), 
-            ) //centrer texte 
-        ), //Scaffold
-      ); //MaterielApp
+          backgroundColor: Colors.black,
+        ),
+        body: Center(
+          child: RichText(
+            text: TextSpan(
+              style: TextStyle(
+                fontSize: 24,
+                ),
+              children: [
+                TextSpan(
+                  text: "Trouvez votre ",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextSpan(
+                  text: "prochain chez-vous",
+                  style: TextStyle(
+                    color: Color(0xFFFFD700), // Couleur or (gold)
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
+
 }
